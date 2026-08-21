5dbae72b96fc33bdbfd61043f220a4c78148dba8
jakell <jalexkell@gmail.com>
2026-08-21 19:48:53 +0100

    Delete flow directory

--- flow/metrics/metrics_abc.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class Metrics(metaclass=ABCMeta):
-    @abstractmethod
-    def write_metric(self, task, action):
-        pass
--- flow/staticqualityanalysis/static_quality_analysis_abc.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class Static_Quality_Analysis(metaclass=ABCMeta):
-    @abstractmethod
-    def scan_code(self):
-        pass
